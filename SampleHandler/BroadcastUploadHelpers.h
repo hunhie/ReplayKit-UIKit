97e2f0b5aad1fb389faeaa0c89b94cd7d1067714
walkerhilla <huny98.dev@gmail.com>
2023-11-23 18:35:29 +0900

    기능 구현 완료

---
+++ SampleHandler/BroadcastUploadHelpers.h
@@ -0,0 +1,14 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#ifndef BroadcastUploadHelpers_h
+#define BroadcastUploadHelpers_h
+
+#import <ReplayKit/ReplayKit.h>
+
+void finishBroadcastGracefully(RPBroadcastSampleHandler * _Nonnull broadcastSampleHandler);
+
+#endif /* BroadcastUploadHelpers_h */
+
+void finishBroadcastGracefully(RPBroadcastSampleHandler * _Nonnull broadcastSampleHandler);
---
+++ SampleHandler/BroadcastUploadHelpers.m
@@ -0,0 +1,8 @@
+//
+//  BroadcastUploadHelpers.m
+//  SampleHandler
+//
+//  Created by walkerhilla on 11/23/23.
+//
+
+#import <Foundation/Foundation.h>
